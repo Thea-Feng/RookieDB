d03c70d468de356150d28485f26f178331ffff23
Thea-Feng <71555910+Thea-Feng@users.noreply.github.com>
2024-10-16 05:54:37 -0500

    3

---
+++ Concurrency/src/main/java/edu/berkeley/cs186/database/concurrency/LockType.java
@@ -0,0 +1,184 @@
+package edu.berkeley.cs186.database.concurrency;
+
+/**
+ * Utility methods to track the relationships between different lock types.
+ */
+public enum LockType {
+    S,   // shared
+    X,   // exclusive
+    IS,  // intention shared
+    IX,  // intention exclusive
+    SIX, // shared intention exclusive
+    NL;  // no lock held
+
+    /**
+     * This method checks whether lock types A and B are compatible with
+     * each other. If a transaction can hold lock type A on a resource
+     * at the same time another transaction holds lock type B on the same
+     * resource, the lock types are compatible.
+     */
+    public static boolean compatible(LockType a, LockType b) {
+        if (a == null || b == null) {
+            throw new NullPointerException("null lock type");
+        }
+        // TODO(proj4_part1): implement
+
+        switch (a) {
+            case S:
+                if( b == NL || b == S || b == IS ) return true;
+                else{
+                    return false;
+                }
+            case X:
+                if(b == NL) return true;
+                else{
+                    return false;
+                }
+            case IS:
+                if( b == NL||b == IS||b == S||b == IX||b == SIX) return true;
+                else{
+                    return false;
+                }
+            case IX:
+                if( b == IX ||b == NL || b == IS) return true;
+                else{
+                    return false;
+                }
+            case SIX:
+                if(b == NL||b==IS) return true;
+                else{
+                    return false;
+                }
+            case NL:
+                return true;
+            default: throw new UnsupportedOperationException("bad lock type");
+        }
+
+    }
+
+    /**
+     * This method returns the lock on the parent resource
+     * that should be requested for a lock of type A to be granted.
+     */
+    public static LockType parentLock(LockType a) {
+        if (a == null) {
+            throw new NullPointerException("null lock type");
+        }
+        switch (a) {
+            case S: return IS;
+            case X: return IX;
+            case IS: return IS;
+            case IX: return IX;
+            case SIX: return IX;
+            case NL: return NL;
+            default: throw new UnsupportedOperationException("bad lock type");
+        }
+    }
+
+    /**
+     * This method returns if parentLockType has permissions to grant a childLockType
+     * on a child.
+     */
+    public static boolean canBeParentLock(LockType parentLockType, LockType childLockType) {
+        if (parentLockType == null || childLockType == null) {
+            throw new NullPointerException("null lock type");
+        }
+        // TODO(proj4_part1): implement
+        switch (parentLockType) {
+            case S:
+                if(childLockType == NL ) return true;
+                return false;
+            case X:
+                if(childLockType == NL ) return true;
+                return false;
+            case IS:
+                if(childLockType == IS|| childLockType == S || childLockType == NL) return true;
+                else{
+                    return false;
+                }
+            case IX:
+                if( childLockType == IX ||childLockType == X || childLockType == IS ||childLockType == S|| childLockType == NL||childLockType == SIX ) return true;
+                else{
+                    return false;
+                }
+            case SIX:
+                if(childLockType == IX ||childLockType == X|| childLockType == NL) return true;
+                else{
+                    return false;
+                }
+            case NL:
+                if(childLockType == NL) return true;
+                return false;
+            default: throw new UnsupportedOperationException("bad lock type");
+        }
+
+    }
+
+    /**
+     * This method returns whether a lock can be used for a situation
+     * requiring another lock (e.g. an S lock can be substituted with
+     * an X lock, because an X lock allows the transaction to do everything
+     * the S lock allowed it to do).
+     */
+    public static boolean substitutable(LockType substitute, LockType required) {
+        if (required == null || substitute == null) {
+            throw new NullPointerException("null lock type");
+        }
+        // TODO(proj4_part1): implement
+        switch (substitute) {
+            case S:
+                if(required == NL||required == S) return true;
+                else{ //ix/x/is
+                    return false;
+                }
+//
+            case X:
+                if(required == NL||required == S||required == X) return true;
+                else{
+                    return false;
+                }
+            case IS:
+                if(required == NL||required == IS) return true;
+                else{
+                    return false;
+                }
+            case IX:
+                if(required == NL||required == IS||required == IX) return true;
+                else{
+                    return false;
+                }
+            case SIX:
+                if(required == NL||required == S||required == SIX) return true;
+                else{
+                    return false;
+                }
+            case NL:
+                return required==NL;
+//                default: return false;
+            default: throw new UnsupportedOperationException("bad lock type");
+        }
+
+
+    }
+
+    /**
+     * @return True if this lock is IX, IS, or SIX. False otherwise.
+     */
+    public boolean isIntent() {
+        return this == LockType.IX || this == LockType.IS || this == LockType.SIX;
+    }
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case S: return "S";
+            case X: return "X";
+            case IS: return "IS";
+            case IX: return "IX";
+            case SIX: return "SIX";
+            case NL: return "NL";
+            default: throw new UnsupportedOperationException("bad lock type");
+        }
+    }
+}
+
